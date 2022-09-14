f8fd08f220bea0289628693a267ba03f9b8b6b8d
= <luke.monahan@tesserent.com>
2022-09-14 16:13:01 +1000

    Update to using Splunk AOB latest version

--- README/inputs.conf.spec
+++ README/inputs.conf.spec
@@ -1,3 +1,3 @@
 [otx://<name>]
-api_key = OTX API Key
+api_key = 
 backfill_days = Number of days to backfill pulses and indicators for the first time the input runs
--- bin/input_module_otx.py
+++ bin/input_module_otx.py
@@ -30,33 +30,39 @@ def collect_events(helper, ew):
     backfill_days = int(helper.get_arg('backfill_days'))
 
     run_time = time.time()
-    
-    last_ran = helper.get_check_point('last_ran')
+
+    last_ran = helper.get_check_point(hashlib.md5(helper.get_input_stanza_names().encode()).hexdigest())
+
+    if last_ran is None:
+      # Might be a migration from old version:
+      last_ran = helper.get_check_point('last_ran')
+      helper.delete_check_point('last_ran')
+
 
     if last_ran is not None:
- 	    since = datetime.utcfromtimestamp(last_ran)
+      since = datetime.utcfromtimestamp(last_ran)
     else:
- 	    since = datetime.now() - timedelta(days = backfill_days)  
- 		
-    helper.log_info("Retrieving subscribed pulses since: %s" % str(since))	
- 		
+      since = datetime.now() - timedelta(days = backfill_days)
+
+    helper.log_info("Retrieving subscribed pulses since: %s" % str(since))
+
     response = helper.send_http_request(
- 	    'https://otx.alienvault.com/api/v1/pulses/subscribed', 
-        'GET', 
-        parameters = {'modified_since' : since }, 
-        headers = { 'X-OTX-API-KEY' : api_key }, 
-        verify=True, 
+      'https://otx.alienvault.com/api/v1/pulses/subscribed',
+        'GET',
+        parameters = {'modified_since' : since },
+        headers = { 'X-OTX-API-KEY' : api_key },
+        verify=True,
         use_proxy=True
     )
-    
+
     response.raise_for_status()
-    
+
     pulses = response.json()['results']
     
     pulse_count = 0
     indicator_count = 0
     for pulse in pulses:
-        
+
         indicators = pulse.pop('indicators', None)
 
         timeparts = pulse['modified'].split('.')
@@ -69,18 +75,18 @@ def collect_events(helper, ew):
             sourcetype = "otx:pulse",
             index = helper.get_output_index(),
             done = True
-		)
+    )
         ew.write_event(e)
-		
+
         pulse_count = pulse_count + 1
 
         for indicator in indicators:
             indicator['pulse_id'] = pulse['id']
-            
+
             timeparts = indicator['created'].split('.')
             time_parsed = utc_to_local(datetime.strptime(timeparts[0], "%Y-%m-%dT%H:%M:%S"))
             xtime = time.mktime(time_parsed.timetuple())
-            
+
             e = helper.new_event(
                 data = json.dumps(indicator),
                 time = xtime,
@@ -89,10 +95,9 @@ def collect_events(helper, ew):
                 done = True
             )
             ew.write_event(e)
-            
+
             indicator_count = indicator_count + 1
 
     helper.log_info("Completed polling. Logged %d pulses and %d indicators." % (pulse_count, indicator_count))
- 		
-    helper.save_check_point('last_ran', run_time)
-    #helper.delete_check_point('last_ran')
+
+    helper.save_check_point(hashlib.md5(helper.get_input_stanza_names().encode()).hexdigest(), run_time)
